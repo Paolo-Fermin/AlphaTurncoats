b4d0246d67004355e37940c016363833395f35aa
Paolo Fermin <paolo.fermin@shield.ai>
2022-11-07 23:21:53 -0500

    initial player class

---
+++ turncoats/player.py
@@ -0,0 +1,17 @@
+"""The player takes actions on the board"""
+from typing import List
+from globals import Stone
+
+class Player:
+
+    def __init__(self, id: str, starting_hand: List[Stone]):
+        """Each player must be initialized with a unique string and a list of 8 stones to start with
+
+        Args:
+            id (str): player id
+        """
+        self.id: str = id
+
+        assert len(starting_hand) == 8, "Must provide 8 stones to start!"
+        self.hand = starting_hand
+
